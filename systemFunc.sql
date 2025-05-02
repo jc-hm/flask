1ee63a94298a173ce859bd65fe1a27c964ebf357
ADMIN <waw-dex-notifications-aaaaiwuw472ok7f7ch4oubogu4@snowflake.org.slack.com>
2025-05-02 00:54:32 +0000

    commit on main

--- systemFunc.sql
+++ systemFunc.sql
@@ -1,4 +1,7 @@
 ALTER WORKSPACE "USER$"."PUBLIC"."jc_test_flask_oauth" PUSH NAME='foo' EMAIL='aloha' COMMENT='aloha';
 
 
-SELECT SYSTEM$WORKSPACE_REPO_COMMIT_AND_PUSH('"USER$"."PUBLIC"."jc_test_flask_oauth', 'commit on main', '', '', '');
+SELECT SYSTEM$WORKSPACE_REPO_COMMIT_AND_PUSH('"USER$.PUBLIC."jc_test_flask_oauth"', 'commit on main', '', '', '');
+
+
+-- aloha
